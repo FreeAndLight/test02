1d30cfab72add4aba2cf80493bd131737c2df49e
Amos <1414366765@qq.com>
2019-01-07 20:30:45 +0800

    添加Java工程到GitHub

--- src/main/java/com/itcast/Demo.java
+++ src/main/java/com/itcast/Demo.java
@@ -3,5 +3,10 @@ package com.itcast;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("123");
+        test();
+    }
+
+    private static void test() {
+
     }
 }
